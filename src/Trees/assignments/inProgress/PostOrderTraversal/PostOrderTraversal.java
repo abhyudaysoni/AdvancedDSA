277795afccb1583286e680d532f27ce9afc7e35a
abhyudaysoni <67017216+abhyudaysoni@users.noreply.github.com>
2022-05-06 00:24:05 +0530

    assignments

--- src/Trees/assignments/inProgress/PostOrderTraversal/PostOrderTraversal.java
+++ src/Trees/assignments/inProgress/PostOrderTraversal/PostOrderTraversal.java
@@ -8,11 +8,66 @@ public class PostOrderTraversal {
         Tree intTree = new Tree();
         int nodes = sc.nextInt();
         for (int i = 0; i < nodes; i++) {
-            int leftVal = sc.nextInt();
-            int rightVal = sc.nextInt();
-            intTree.insert(leftVal);
-            intTree.insert(rightVal);
+            intTree.insert(sc.nextInt());
+            intTree.insert(sc.nextInt());
         }
         intTree.displayPostOrderTraversal();
+    }
+
+    public static class Node {
+        int data;
+        Node leftChild;
+        Node rightChild;
+
+        public Node(int data) {
+            this.data = data;
+        }
+
+        public void postOrderTraversal() {
+            if (leftChild != null) {
+                leftChild.postOrderTraversal();
+            }
+            if (rightChild != null) {
+                rightChild.postOrderTraversal();
+            }
+            System.out.print(data + " ");
+        }
+
+        public void insert(int val) {
+            if (val == data) {
+                return;
+            }
+            if (val < data) {
+                if (leftChild == null) {
+                    leftChild = new Node(val);
+                } else {
+                    leftChild.insert(val);
+                }
+            } else {
+                if (rightChild == null) {
+                    rightChild = new Node(val);
+                } else {
+                    rightChild.insert(val);
+                }
+            }
+        }
+    }
+
+    public static class Tree {
+        private Node root;
+
+        public void insert(int val) {
+            if (root == null) {
+                root = new Node(1);
+            } else {
+                root.insert(val);
+            }
+        }
+
+        public void displayPostOrderTraversal() {
+            if (root != null) {
+                root.postOrderTraversal();
+            }
+        }
     }
 }
